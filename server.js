fac8768998de9bad0cf6941a754670bdc0e4495c
jesusgabriel <amelo@fvi.edu>
2017-03-01 20:56:46 -0500

    save

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 2999;
 
 app.get('/', function(req, res){
-  res.send('It is working my server')
+  res.send('HELLO WORD')
 });
 app.listen(PORT, function(){
   console.log("server listenig now");
